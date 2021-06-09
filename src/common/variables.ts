a6371df82c7c68148523f207cfc69deebce64ff5
gwaksuyeon <kwak7663@gmail.com>
2021-06-09 17:59:08 +0900

    menu add

--- src/common/variables.ts
+++ src/common/variables.ts
@@ -2,4 +2,9 @@ export const ROUTE_ROOT = {
   MAIN: '/',
   ROULETTE: '/roulette',
   SHARE: '/share',
+  PAGINATION: '/pagination',
+  INFINITY_SCROLL_PAGING: '/infinite/scroll',
+  INFINITY_PAGING: '/infinite/paging',
+  ROLES: '/roles',
+  EDITOR: '/editor',
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -12,6 +12,11 @@ const Main: React.FC = () => {
       <Contents>
         <Menu title={"룰렛"} link={ROUTE_ROOT.ROULETTE} />
         <Menu title={"공유하기"} link={ROUTE_ROOT.SHARE} />
+        <Menu title={"페이징"} link={ROUTE_ROOT.PAGINATION} />
+        <Menu title={"무한스크롤"} link={ROUTE_ROOT.INFINITY_SCROLL_PAGING} />
+        <Menu title={"무한페이징"} link={ROUTE_ROOT.INFINITY_PAGING} />
+        <Menu title={"접속권한"} link={ROUTE_ROOT.ROLES} />
+        <Menu title={"에디터"} link={ROUTE_ROOT.EDITOR} />
       </Contents>
     </Container>
   );
