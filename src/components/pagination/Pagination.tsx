08ce8033e265411062f37309d9ce616006be4716
gwaksuyeon <kwak7663@gmail.com>
2021-06-11 15:19:27 +0900

    [2021-06-11] 페이징 로직 추가

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 
 interface Props {
@@ -16,96 +16,60 @@ const Pagination: React.FC<Props> = ({
   page, // 현재 페이지
   onClickPage,
 }) => {
-  const totalPageCount = Math.ceil(totalCount / limit); // 총 페이지 수
-  const [pageGroup, setPageGroup] = useState<number>(1);
-  const [lastPageNumber, setLastPageNumber] = useState<number>(0);
-  const [firstPageNumber, setFirstPageNumber] = useState<number>(0);
-  const [nextPageNumber, setNextPageNumber] = useState<number>(0);
-  const [prevPageNumber, setPrevPageNumber] = useState<number>(0);
+  const pageGroup = Math.ceil(page / pageLimit);
+  const totalPage = Math.ceil(totalCount / limit);
+  const totalPageList = new Array(totalPage)
+    .fill(0)
+    .map((obj: any, inx: number) => inx + 1);
 
-  const onCalculation = (currentPage: number) => {
-    const pageGroupCnt = Math.ceil(currentPage / pageLimit);
-
-    let last = pageGroupCnt * pageLimit;
-    let first = (pageGroupCnt - 1) * pageLimit + 1;
-    if (last > totalPageCount) {
-      last = totalPageCount;
-    }
-
-    let next = last + 1;
-    let prev = first - 1;
-
-    setPageGroup(pageGroupCnt);
-    setLastPageNumber(last);
-    setFirstPageNumber(first);
-    setNextPageNumber(next);
-    setPrevPageNumber(prev);
-  };
-
-  const onClickPrevButton = () => {
-    if (prevPageNumber === 0) {
-      onClickPage(0);
-      onCalculation(1);
-    } else {
-      onClickPage(firstPageNumber - pageLimit - 1);
-      onCalculation(prevPageNumber);
-    }
-  };
-
-  const onClickNextButton = () => {
-    if (nextPageNumber > totalPageCount) {
-      onClickPage(totalPageCount - 1);
-      onCalculation(totalPageCount);
-    } else {
-      onClickPage(nextPageNumber - 1);
-      onCalculation(nextPageNumber);
-    }
-  };
-
-  const onClickStartButton = () => {
+  const onClickLastPrev = () => {
     onClickPage(0);
-    onCalculation(1);
   };
 
-  const onClickEndButton = () => {
-    onClickPage(totalPageCount - 1);
-    onCalculation(totalPageCount);
-  };
-
-  useEffect(() => {
-    if (totalPageCount) {
-      onCalculation(page + 1);
+  const onClickPrev = () => {
+    if (page - 1 < 0) {
+      onClickPage(0);
+    } else {
+      onClickPage(page - 1);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [totalPageCount]);
+  };
+
+  const onClickNext = () => {
+    if (page + 1 > totalPage - 1) {
+      onClickPage(totalPage - 1);
+    } else {
+      onClickPage(page + 1);
+    }
+  };
+
+  const onClickLastNext = () => {
+    onClickPage(totalPage - 1);
+  };
 
   return (
     <Container>
-      <Button
-        onClick={prevPageNumber === 0 ? () => {} : onClickStartButton}
-      >{`<<`}</Button>
-      <Button
-        onClick={prevPageNumber === 0 ? () => {} : onClickPrevButton}
-      >{`<`}</Button>
-      {new Array(lastPageNumber)
-        .fill(0)
-        .slice(firstPageNumber - 1)
+      <Button disabled={page === 0} onClick={onClickLastPrev}>{`<<`}</Button>
+      <Button disabled={page === 0} onClick={onClickPrev}>{`<`}</Button>
+
+      {totalPageList
+        .slice(pageGroup, pageGroup + pageLimit)
         .map((obj: any, inx: number) => (
           <Button
-            key={`page-${inx}`}
-            active={page === inx + (pageGroup - 1) * pageLimit}
-            onClick={
-              page === inx + (pageGroup - 1) * pageLimit
-                ? () => {}
-                : () => onClickPage(inx + (pageGroup - 1) * pageLimit)
-            }
+            key={`pagination-${inx}`}
+            active={obj === page + 1}
+            onClick={() => onClickPage(obj - 1)}
           >
-            {inx + 1 + (pageGroup - 1) * pageLimit}
+            {obj}
           </Button>
         ))}
-      <Button onClick={onClickNextButton}>{`>`}</Button>
+
       <Button
-        onClick={nextPageNumber > totalPageCount ? () => {} : onClickEndButton}
+        disabled={page === totalPage - 1}
+        onClick={onClickNext}
+      >{`>`}</Button>
+      <Button
+        disabled={page === totalPage - 1}
+        onClick={onClickLastNext}
       >{`>>`}</Button>
     </Container>
   );
@@ -127,7 +91,8 @@ const Button = styled.div<any>`
   border-radius: 2px;
   background: ${(props: any) => props.active && "#e9edf1"};
   font-weight: ${(props: any) => props.active && "bold"};
-  cursor: pointer;
+  color: ${(props: any) => props.disabled && "#ddd"};
+  cursor: ${(props: any) => (props.disabled ? "not-allowed" : "pointer")};
 
   & ~ & {
     margin-left: 4px;
