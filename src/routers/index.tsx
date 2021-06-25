bd3082b1a7cd18b99bdeeaa06bcdc83bbf19b943
gwaksuyeon <kwak7663@gmail.com>
2021-06-25 11:00:42 +0900

    [2021-06-25] router 재설정

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -22,36 +22,68 @@ const Root: React.FC = () => {
     return (
         <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
             <Switch>
-                <Route path={ROUTE_ROOT.MAIN} exact component={Main} />
-                <Route path={ROUTE_ROOT.ROULETTE} exact component={Roulette} />
-                <Route path={ROUTE_ROOT.SHARE} exact component={Share} />
                 <Route
-                    path={ROUTE_ROOT.PAGINATION}
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.MAIN}`}
+                    exact
+                    component={Main}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.ROULETTE}`}
+                    exact
+                    component={Roulette}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.SHARE}`}
+                    exact
+                    component={Share}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.PAGINATION}`}
                     exact
                     component={Pagination}
                 />
                 <Route
-                    path={ROUTE_ROOT.INFINITY_SCROLL_PAGING}
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.INFINITY_SCROLL_PAGING}`}
                     exact
                     component={InfiniteScroll}
                 />
                 <Route
-                    path={ROUTE_ROOT.INFINITY_PAGING}
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.INFINITY_PAGING}`}
                     exact
                     component={InfiniteButton}
                 />
-                <Route path={ROUTE_ROOT.ROLES} exact component={Roles} />
-                <Route path={ROUTE_ROOT.EDITOR} exact component={Editor} />
-                <Route path={ROUTE_ROOT.REORDER} exact component={Reorder} />
-                <Route path={ROUTE_ROOT.CSV} exact component={CSV} />
-                <Route path={ROUTE_ROOT.CHART} exact component={Chart} />
                 <Route
-                    path={ROUTE_ROOT.POPUP_NOTICE}
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.ROLES}`}
+                    exact
+                    component={Roles}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.EDITOR}`}
+                    exact
+                    component={Editor}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB}${ROUTE_ROOT.REORDER}`}
+                    exact
+                    component={Reorder}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB} ${ROUTE_ROOT.CSV}`}
+                    exact
+                    component={CSV}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB} ${ROUTE_ROOT.CHART}`}
+                    exact
+                    component={Chart}
+                />
+                <Route
+                    path={`${ROUTE_ROOT.GIT_HUB} ${ROUTE_ROOT.POPUP_NOTICE}`}
                     exact
                     component={PopupNotice}
                 />
                 <Route
-                    path={ROUTE_ROOT.GOODS_OPTION}
+                    path={`${ROUTE_ROOT.GIT_HUB} ${ROUTE_ROOT.GOODS_OPTION}`}
                     exact
                     component={GoodsOption}
                 />
